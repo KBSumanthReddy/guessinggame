424ce5bdf68cbeea85dcfaf920dce098e07bade1
KBSumanthReddy <117896310+KBSumanthReddy@users.noreply.github.com>
2022-12-30 12:53:29 +0530

    Update external.js

--- external.js
+++ external.js
@@ -1,21 +1,35 @@
-alert("Hello");
-alert("Welcome to Guessing game");
-alert("Let's Start");
-x = Math.floor(Math.random() * 100);
-let n=0,a;
-do{
-    a = prompt("guess a number?");
-    a = parseInt(a)
-    if(a>x && a<=100){alert("your number is greater");}
-    else if(a<x && a<=100){alert("your number is lesser");}
+let score = document.getElementById("score");
+let input = document.getElementsByTagName("input");
+let para = document.getElementById("para");
+let button = document.getElementById("button");
+para.style.marginLeft= 'auto';
+para.style.marginRight = 'auto';
+para.style.fontSize = '20px';
+let x = Math.floor(Math.random() * 100);
+let n=0;
+function game(){
+    let a;
+    a = parseInt(input[0].value);
+    if(n>=10){
+        alert("Game Over")
+        para.innerHTML = "Better Luck Next time";
+        input.value = "";
+    }
+    else if(a>x && a<=100){
+        para.innerHTML = `your number(${a}) is greater`;
+        n++;
+    }
+    else if(a<x && a<=100){
+        para.innerHTML = `your number(${a}) is lesser`;
+        n++;
+    }
     else if(a==x){
-        alert("you guessed correct");
-        alert("your score is")
-        alert(100-n);
-        document.write("your score is ",100-n);
+        alert(`you won\nyour score is ${10-n}`);
+        para.innerHTML = `your score is ${10-n}`;
+        x = Math.floor(Math.random() * 100);
+        n=0;
     }
     else{
         alert("input is wrong,try again");
     }
-    n++;
-}while(a!=x)
+}
